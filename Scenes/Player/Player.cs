6839726249b7fac721c928e559fbbbadc5af1170
dreischild <ds@dreischild.com>
2025-09-13 08:46:43 +0200

    Refaktorisierung

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -14,33 +14,42 @@ public partial class Player : CharacterBody2D
 	}
 
 	public override void _PhysicsProcess(double delta)
-	{
-		/** https://docs.godotengine.org/de/4.x/tutorials/2d/2d_movement.html **/
-		Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down") * moveSpeed;
+    {
+        Move();
+        Animate();
+    }
 
-		Velocity = inputDirection;
-		MoveAndSlide();
+    private void Move()
+    {
+        /** https://docs.godotengine.org/de/4.x/tutorials/2d/2d_movement.html **/
+        Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down") * moveSpeed;
 
-		/** https://docs.godotengine.org/en/4.4/tutorials/2d/2d_sprite_animation.html **/
-		if (Input.IsActionPressed("move_left"))
-		{
-			animatedSprite.Play("move_left");
-		}
-		else if (Input.IsActionPressed("move_right"))
-		{
-			animatedSprite.Play("move_right");
-		}
-		else if (Input.IsActionPressed("move_up"))
-		{
-			animatedSprite.Play("move_up");
-		}
-		else if (Input.IsActionPressed("move_down"))
-		{
-			animatedSprite.Play("move_down");
-		}
-		else
-		{
-			animatedSprite.Stop();
-		}
-	}
+        Velocity = inputDirection;
+        MoveAndSlide();
+    }
+
+    private void Animate()
+    {
+        /** https://docs.godotengine.org/en/4.4/tutorials/2d/2d_sprite_animation.html **/
+        if (Input.IsActionPressed("move_left"))
+        {
+            animatedSprite.Play("move_left");
+        }
+        else if (Input.IsActionPressed("move_right"))
+        {
+            animatedSprite.Play("move_right");
+        }
+        else if (Input.IsActionPressed("move_up"))
+        {
+            animatedSprite.Play("move_up");
+        }
+        else if (Input.IsActionPressed("move_down"))
+        {
+            animatedSprite.Play("move_down");
+        }
+        else
+        {
+            animatedSprite.Stop();
+        }
+    }
 }
